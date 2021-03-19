7038225dad7eeec45adc4b1813227524dad3f0a8
Kevin Manuel <kevin.manueldx@hotmail.com>
2021-03-20 01:35:25 +0800

    add log

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,7 +21,9 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -42,9 +46,9 @@ public class UserController {
 	
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
-		//add log?
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
+		log.info("username set with " + user.getUsername());
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
@@ -60,19 +64,14 @@ public class UserController {
 		}
 
 		if(!isIdenticalPassword || !isPasswordLengthAtLeast8){
-			//log error?
+			log.error("bad password");
 			return ResponseEntity.badRequest().build();
 		}
 
-//		if(createUserRequest.getPassword().length()<7 ||
-//				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
-//			//System.out.println("Error - Either length is less than 7 or pass and conf pass do not match. Unable to create ",
-//			//		createUserRequest.getUsername());
-//			return ResponseEntity.badRequest().build();
-//		}
 		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 
 		userRepository.save(user);
+		log.info("user created");
 		return ResponseEntity.ok(user);
 	}
 	
